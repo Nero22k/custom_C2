0c06c8f32306c8375b22df066e4d1edf8e1ea396
Nero22k <64486541+Nero22k@users.noreply.github.com>
2021-11-07 15:28:10 +0000

    Several improvements and fixes

--- implant/results.cpp
+++ implant/results.cpp
@@ -7,7 +7,7 @@
 
 // Result object returned by all tasks
 // Includes the task ID, result contents and success status (true/false)
-Result::Result(const boost::uuids::uuid& id,
+Result::Result(const std::string& id,
 	std::string contents,
 	const bool success)
 	: id(id), contents{ std::move(contents) }, success(success) {}
